9958245a22df1256f66390886eb54442f341cde8
Mdhussaindev <mdhussain.dev00@gmail.com>
2026-08-22 15:38:58 +0500

    update

---
+++ src/components/Layout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import "./MainLayout.css";
+
+function MainLayout() {
+  return (
+    <div className="app-layout">
+      <main className="main-content">
+        <h1>PlayVerse</h1>
+      </main>
+    </div>
+  );
+}
+
+export default MainLayout;
