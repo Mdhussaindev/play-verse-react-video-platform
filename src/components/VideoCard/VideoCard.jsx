71cd0b3dfd98d2723dc74a960dbd553d03454509
Mdhussaindev <mdhussain.dev00@gmail.com>
2026-09-02 00:32:02 +0500

    update

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,37 +1,58 @@
 import "./VideoCard.css";
 
+const profileImage =
+  "https://i.pravatar.cc/100?img=12";
+
 function VideoCard({ video }) {
   return (
     <article className="video-card">
+
+      {/* Thumbnail */}
       <div className="video-thumbnail-wrapper">
+
         <img
           src={video.thumbnail}
           alt={video.title}
           className="video-thumbnail"
         />
+
+        {/* Video Duration - ALWAYS VISIBLE */}
+        <span className="video-duration">
+          {video.duration}
+        </span>
+
       </div>
 
+      {/* Video Information */}
       <div className="video-info">
+
+        {/* Channel Avatar */}
         <img
-          src={video.avatar}
-          alt={video.channel}
+          src={profileImage}
+          alt="Hussain Khan"
           className="video-avatar"
         />
 
         <div className="video-details">
+
+          {/* Title */}
           <h3>{video.title}</h3>
 
+          {/* Channel */}
           <p className="video-channel">
             {video.channel}
           </p>
 
+          {/* Views + Upload Time */}
           <p className="video-meta">
-            {video.views}
-            <span>•</span>
-            {video.date}
+            <span>{video.views}</span>
+            <span className="meta-dot">•</span>
+            <span>{video.date}</span>
           </p>
+
         </div>
       </div>
+
     </article>
   );
 }
