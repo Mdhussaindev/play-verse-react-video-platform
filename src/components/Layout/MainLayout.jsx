2ec95316d9be31b42b50da172154d8b94aa88ae1
Mdhussaindev <mdhussain.dev00@gmail.com>
2026-09-02 00:54:33 +0500

    update

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -1,3 +1,27 @@
+// import Header from "../Header/Header";
+// import Sidebar from "../Sidebar/Sidebar";
+// import VideoGrid from "../VideoGrid/VideoGrid";
+
+// import "./MainLayout.css";
+
+// function MainLayout() {
+//   return (
+//     <div className="app-layout">
+//       <Header />
+
+//       <div className="app-body">
+//         <Sidebar />
+
+//         <main className="main-content">
+//           <VideoGrid />
+//         </main>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default MainLayout;
+
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
 import VideoGrid from "../VideoGrid/VideoGrid";
@@ -7,15 +31,19 @@ import "./MainLayout.css";
 function MainLayout() {
   return (
     <div className="app-layout">
+
       <Header />
 
       <div className="app-body">
+
         <Sidebar />
 
         <main className="main-content">
           <VideoGrid />
         </main>
+
       </div>
+
     </div>
   );
 }
